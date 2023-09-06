0c57aaf345912ba63ed3ab4a2c8db031128552ea
Alex <in76gener@gmail.com>
2023-09-06 23:07:25 +0300

    Modify chat.js

--- GroupChat/src/main/java/main/ChatController.java
+++ GroupChat/src/main/java/main/ChatController.java
@@ -1,13 +1,50 @@
 package main;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class ChatController {
 
     @GetMapping("/init")
-    public String init(){
-        return "YES";
+    public HashMap<String, Boolean> init(){
+
+        HashMap<String, Boolean> response = new HashMap<>();
+        //TODO: check sessionId. If found => true, if not => false
+
+        response.put("result", true);
+        return response;
+    }
+
+    @PostMapping("/auth")
+    public HashMap<String, Boolean> auth(@RequestParam String name){
+
+        HashMap<String, Boolean> response = new HashMap<>();
+        //TODO:
+        // - create User with name, sessionId
+        // - save User to DB
+
+        response.put("result", true);
+        return response;
+    }
+
+    @PostMapping("/message")
+    public Boolean sendMessage(@RequestParam String message) {
+        return true;
+    }
+
+    @GetMapping("/message")
+    public List<String> getMessagesList(){
+        return null;
+    }
+
+    @GetMapping("/user")
+    public HashMap<Integer, String> getUserList(){
+        return null;
     }
 }
