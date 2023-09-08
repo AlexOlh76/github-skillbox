2ec9af53ca9f9c616b4cab230efba5556fcd4908
Alex <in76gener@gmail.com>
2023-09-08 23:27:35 +0300

    add auth method

--- GroupChat/src/main/java/main/ChatController.java
+++ GroupChat/src/main/java/main/ChatController.java
@@ -1,15 +1,23 @@
 package main;
 
+import main.model.User;
+import main.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.servlet.mvc.condition.RequestConditionHolder;
 
 import java.util.HashMap;
 import java.util.List;
 
 @RestController
 public class ChatController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/init")
     public HashMap<String, Boolean> init(){
@@ -28,6 +36,14 @@ public class ChatController {
         //TODO:
         // - create User with name, sessionId
         // - save User to DB
+        String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
+        User user = new User();
+        user.setName(name);
+        user.setSessionId(sessionId);
+
+        userRepository.save(user);
+
+
 
         response.put("result", true);
         return response;
